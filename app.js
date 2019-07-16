4039229d33062aed96b3935b38093d568954ccc2
FunctionJoo <qufstar46@gmail.com>
2019-07-16 15:39:54 +0900

    create bootstrap 07

--- app.js
+++ app.js
@@ -43,6 +43,11 @@ res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/bootstrap06.html'
 //__dirname : It will resolve to your project folder.
 });
 
+router.get('/bootstrap07_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/bootstrap07.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+    
 
 //static links for Junguk
 //Your code goes here...
