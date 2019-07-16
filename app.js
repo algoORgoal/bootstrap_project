e1ffc154a42bc736a07dcb09b312d7dad31893e2
FunctionJoo <qufstar46@gmail.com>
2019-07-16 12:58:33 +0900

    add bootstrap folders

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ app.use(express.static('public'))
 
 const path = require('path');
 const router = express.Router();
+
 router.get('/', function (req, res) {
 res.sendFile(path.join(__dirname + '/index.html'));
 //__dirname : It will resolve to your project folder.
 });
+
+//static links for Byeolchan
+router.get('/byeolchan/boostrap01', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/bootstrap01.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+//static links for Junguk
+//Your code goes here...
 
 app.use('/', router);
 
